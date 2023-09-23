822705d9049d04ba4fa92dfeeec511be8e132f5b
dimasikqqq <dima.eflanov@gmail.com>
2023-09-23 10:39:34 +0500

    pr5

--- pr 5/index.php
+++ pr 5/index.php
@@ -1,20 +1,45 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ <?php 
 $host = "db4.myarena.ru";
 $username = "u19978_b12";
 $password = "xJ8zL3vK6b";
 $dbname = "u19978_b12";
 
-try {
+    try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     // Устанавливаем режим обработки ошибок PDO
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Подключение успешно";
+        echo "Подключение успешно";
 } 
 
-catch(PDOException $e) {
-    echo "Ошибка подключения: " . $e->getMessage();
+    catch(PDOException $e) {
+        echo "Ошибка подключения: " . $e->getMessage();
 }
 
-
-
-?>
+        echo "<table class= 'table' border = '2'>";
+    $result = $conn->query('SELECT * FROM `product`');
+        echo "<tr>"; 
+    for($i - 0; $i < $result->columnCount();$i++){ 
+        $column = $result->getColumnMeta($i); 
+        echo "<th>{$column['name']}</th>"; 
+} 
+        echo "</tr>";
+    while($row = $result->fetch( PDO::FETCH_ASSOC )){
+        echo "<tr>";
+    foreach ($row as $key => $value) {
+        echo "<td>".$value."</td>";
+}
+        echo "</tr>";
+}
+        echo "</table>";
+?> 
+</body>
+</html>
