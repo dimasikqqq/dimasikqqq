2d36939bfbc2ec87ff7928d442622533ac46dcd6
dimasikqqq <dima.eflanov@gmail.com>
2023-10-06 10:17:54 +0500

    Update index.php

--- pr 5/index.php
+++ pr 5/index.php
@@ -7,39 +7,50 @@
     <title>Document</title>
 </head>
 <body>
+
  <?php 
 $host = "db4.myarena.ru";
 $username = "u19978_b12";
 $password = "xJ8zL3vK6b";
 $dbname = "u19978_b12";
-
     try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    // Устанавливаем режим обработки ошибок PDO
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Подключение успешно";
+    $connection = new PDO('mysql:host=' . $host . ';dbname=' . $dbname . ';charset=utf8', $username, $password);
+$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Подключение успешно";
 } 
 
-    catch(PDOException $e) {
-        echo "Ошибка подключения: " . $e->getMessage();
-}
+    catch (PDOException $e) {
+    die("Ошибка подключения: " . $e->getMessage());
+    }
 
-        echo "<table class= 'table' border = '2'>";
-    $result = $conn->query('SELECT * FROM `product`');
-        echo "<tr>"; 
-    for($i - 0; $i < $result->columnCount();$i++){ 
-        $column = $result->getColumnMeta($i); 
-        echo "<th>{$column['name']}</th>"; 
-} 
-        echo "</tr>";
-    while($row = $result->fetch( PDO::FETCH_ASSOC )){
-        echo "<tr>";
-    foreach ($row as $key => $value) {
-        echo "<td>".$value."</td>";
-}
-        echo "</tr>";
-}
-        echo "</table>";
+    $result = $connection->query('SELECT * FROM  `product` WHERE 1');
+
+    if ($result){
+    if ($result->rowCount() == 0) {
+        echo "Запрос не вернул результатов.";
+    } else {
+
+    echo "<table border='1'>";
+        // Вывод заголовков таблицы
+    echo "<tr>";
+        for ($i = 0; $i < $result->columnCount(); $i++) {
+            $column = $result->getColumnMeta($i);
+            echo "<th>{$column['name']}</th>";
+        }
+    echo "</tr>";
+        
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            echo "<tr>";
+            foreach ($row as $value) {
+                echo "<td>".$value."</td>";
+        }
+    echo "</tr>";
+        }
+    echo "</table>";
+        }}
+    else {
+    echo "Запросвыполнен с ошибкой ";
+        }
 ?> 
 </body>
 </html>
