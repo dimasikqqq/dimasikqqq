955e5e1b81cfe1248798297008feb43ce570413c
dimasikqqq <dima.eflanov@gmail.com>
2023-10-07 10:55:17 +0500

    Create index.php

---
+++ pr 7/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+     <form action="action.php" method="post">
+     <p>Login:<span class="error">* <input type="text" name="login"></p>     
+     <p>Password:<span class="error">* <input type="password" name="password"></p>
+     <p>Repeate password:<span class="error">* <input type="password" name="passwordtry"></p>
+     <p>Your mail:<span class="error">* <input type="email" name="Email"></p>
+     <p><input type="submit" value="Register"></p> 
+     </form>
+
+</body>
+</html>
