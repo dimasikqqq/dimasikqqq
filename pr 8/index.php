587eb747e95257d60a635c51143bbb77748b26b7
dimasikqqq <dima.eflanov@gmail.com>
2023-10-13 10:45:03 +0500

    Create index.php

---
+++ pr 8/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h3>Авторизуйтесь<h3>
+     <p><span class="error"> * Обязательное поле </span></p>
+     <form action="action.php" method="post">
+     <p>Логин:<span class="error">* <input type="text" name="login"></p>     
+     <p>Пароль:<span class="error">* <input type="password" name="password"></p>
+     <p><input type="submit" value="Войти"></p> 
+     </form>
+     
+</body>
+</html>
